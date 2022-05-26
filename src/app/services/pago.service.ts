fd1c634eb5effd480ab80717914ad27abd3e25aa
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-05-26 00:19:39 -0500

    Pago Service

--- src/app/services/pago.service.ts
+++ src/app/services/pago.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+import { Pago } from '../interfaces/Pagos';
+import { UiServiceService } from './ui-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagoService {
 
-  constructor() { }
+  pagos: Pago[] = [];
+
+  constructor(private storage: Storage,
+              private uiService: UiServiceService) {
+    this.storage.create();
+    this.cargarPagos();
+  }
+
+  async guardarPago( pago: Pago ){
+    this.pagos.unshift( pago );
+    await this.storage.set('pagos', this.pagos );
+    this.uiService.presentToast('Se agregó a pagos');
+  }
+
+  async cargarPagos(){
+    const pagos = await this.storage.get('pagos');
+    if ( pagos ){
+      this.pagos = pagos;
+    }
+  }
 }
