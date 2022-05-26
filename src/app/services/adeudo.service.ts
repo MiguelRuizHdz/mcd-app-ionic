bf7849e11f4404c0e83fce96a2afd79c7f7c954e
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-05-26 00:19:27 -0500

    Adeudo Service

--- src/app/services/adeudo.service.ts
+++ src/app/services/adeudo.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+import { Adeudo } from '../interfaces/Adeudos';
+import { UiServiceService } from './ui-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdeudoService {
 
-  constructor() { }
+  adeudos: Adeudo[] = [];
+
+  constructor(private storage: Storage,
+              private uiService: UiServiceService) {
+    this.storage.create();
+    this.cargarAdeudos();
+  }
+
+  async guardarAdeudo( adeudo: Adeudo ){
+    this.adeudos.unshift( adeudo );
+    await this.storage.set('adeudos', this.adeudos );
+    this.uiService.presentToast('Se agregó a adeudos');
+  }
+
+  async cargarAdeudos(){
+    const adeudos = await this.storage.get('adeudos');
+    if ( adeudos ){
+      this.adeudos = adeudos;
+    }
+  }
 }
