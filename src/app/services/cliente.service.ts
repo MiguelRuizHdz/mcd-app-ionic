623643e93bf1ddf6908daa1ecfa6798f40a8fe1f
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-05-26 00:11:22 -0500

    Customer Service

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { Cliente } from '../interfaces/Clientes';
+import { UiServiceService } from './ui-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  clientes: Cliente[] = [];
+
+  constructor(private storage: Storage,
+              private uiService: UiServiceService) {
+    this.storage.create();
+    this.cargarClientes();
+  }
+
+  async guardarCliente( cliente: Cliente ){
+    this.clientes.unshift( cliente );
+    await this.storage.set('clientes', this.clientes );
+    this.uiService.presentToast('Se agregó a clientes');
+  }
+
+  async cargarClientes(){
+    const clientes = await this.storage.get('clientes');
+    if ( clientes ){
+      this.clientes = clientes;
+    }
+  }
+
 }
